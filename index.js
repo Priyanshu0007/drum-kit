8a78ef7bb4a54a1260099d71a907555e9e554073
Priyanshu Gupta <priyanshu2859@gmail.com>
2021-11-15 18:36:16 +0530

    drum kit

---
+++ index.js
@@ -0,0 +1,56 @@
+
+for(var n=0; n<document.querySelectorAll(".drum").length;n++){
+ document.querySelectorAll(".drum")[n].addEventListener("click",function(){
+   var buttonInnerHTML=this.innerHTML;
+   makeSounds(buttonInnerHTML);
+   buttonAnimation(buttonInnerHTML);
+ } );
+}
+ document.addEventListener("keypress",function(event){
+  makeSounds(event.key);
+  buttonAnimation(event.key);
+ } );
+
+function makeSounds(key){
+
+switch (key) {
+ case "w":
+     var audio =new Audio("sounds/crash.mp3");
+     audio.play();
+   break;
+ case "a":
+     var audio =new Audio("sounds/kick-bass.mp3");
+     audio.play();
+   break;
+ case "s":
+     var audio =new Audio("sounds/snare.mp3");
+     audio.play();
+   break;
+ case "d":
+     var audio =new Audio("sounds/tom-1.mp3");
+     audio.play();
+   break;
+ case "j":
+     var audio =new Audio("sounds/tom-2.mp3");
+     audio.play();
+   break;
+ case "k":
+     var audio =new Audio("sounds/tom-3.mp3");
+     audio.play();
+   break;
+ case "l":
+     var audio =new Audio("sounds/tom-4.mp3");
+     audio.play();
+   break;
+ default: console.console.log(key);
+
+}
+
+}
+function buttonAnimation(currentKey){
+  var activeButton = document.getElementsByClassName(currentKey);
+  activeButton[0].classList.add("pressed");
+  setTimeout(function(){
+    activeButton[0].classList.remove("pressed");
+  },100);
+  }
